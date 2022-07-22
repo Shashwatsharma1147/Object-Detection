fa89f744ed9e137cb58821cb8598946201d6e3b2
Shashwatsharma1147 <76209043+Shashwatsharma1147@users.noreply.github.com>
2022-07-22 17:59:58 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,31 @@
+img = "";
+Status="";
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("COCOSSD", modelLoaded);
+    document.getElementById("status").innerHTML = "status = detecting object";
+}
+function modelLoaded(){
+    console.log("model is loaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, result){
+    if(error){
+        console.log("Error");
+    }
+    else{
+        console.log(result);
+    }
+}
+
+function preload(){
+    img = loadImage("kitchen.webp");
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
